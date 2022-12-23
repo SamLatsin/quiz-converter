48ec44d9e6716303df619115b6530a4b2e3b2f58
Sonsoles López Pernas <slopezpe@VPK4G9262M.local>
2022-12-23 20:08:03 +0100

    Update docs

---
+++ docs/precache-manifest.c50c1295930e4ce0d473b99e67da01ce.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "bdfb64c2406636b0d90c",
+    "url": "/quiz-converter/static/css/main.2859e2dd.chunk.css"
+  },
+  {
+    "revision": "bdfb64c2406636b0d90c",
+    "url": "/quiz-converter/static/js/main.bdfb64c2.chunk.js"
+  },
+  {
+    "revision": "f5ae269df9790bef24ae",
+    "url": "/quiz-converter/static/js/1.f5ae269d.chunk.js"
+  },
+  {
+    "revision": "a9a59ff2e03cc2d42dc9",
+    "url": "/quiz-converter/static/js/runtime~main.a9a59ff2.js"
+  },
+  {
+    "revision": "3a4a7dd33a40827d5f84c5f2e5a5bfb3",
+    "url": "/quiz-converter/index.html"
+  }
+];
--- docs/service-worker.js
+++ docs/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "./precache-manifest.6762c4bff7a74475bb74f0e6f2d6cec2.js"
+  "/quiz-converter/precache-manifest.c50c1295930e4ce0d473b99e67da01ce.js"
 );
 
 workbox.clientsClaim();
@@ -28,7 +28,7 @@ self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.suppressWarnings();
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute("./index.html", {
+workbox.routing.registerNavigationRoute("/quiz-converter/index.html", {
   
   blacklist: [/^\/_/,/\/[^/]+\.[^/]+$/],
 });

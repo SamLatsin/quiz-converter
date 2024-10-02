a65295754fe6bb1e1487cd3c7d85c6240bf6707c
SamLatsin <37497816+SamLatsin@users.noreply.github.com>
2024-10-02 14:53:56 +0300

    Updates

---
+++ precache-manifest.9847a12253245d1c8577d7292eb6a9f6.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "c594c35b05bcfe958b4d",
+    "url": "/quiz-converter/static/css/main.cabdd3cf.chunk.css"
+  },
+  {
+    "revision": "c594c35b05bcfe958b4d",
+    "url": "/quiz-converter/static/js/main.c594c35b.chunk.js"
+  },
+  {
+    "revision": "f5ae269df9790bef24ae",
+    "url": "/quiz-converter/static/js/1.f5ae269d.chunk.js"
+  },
+  {
+    "revision": "a9a59ff2e03cc2d42dc9",
+    "url": "/quiz-converter/static/js/runtime~main.a9a59ff2.js"
+  },
+  {
+    "revision": "88b91b5f4e32ab09bab8b5269aeebadc",
+    "url": "/quiz-converter/index.html"
+  }
+];
